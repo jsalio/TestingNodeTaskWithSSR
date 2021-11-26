3dbb846923b8dba880f37e78269313e9110e67a0
jrodriguez <jrodriguez@novosit.com>
2021-11-26 09:59:00 -0400

    Render ssr

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,27 @@
 import { runningTask } from './filePath';
+import express from 'express';
+import compression from 'compression';
+const app = express();
+import ssr from "./route/ssr";
+
+app.use(compression());
+app.use(express.static('public'));
+app.use('/firstssr', ssr)
+
+app.get('/', (req, res) => {
+    const date = new Date();
+    res.send(`This a response from Express JS  at ${date}`);
+});
 
 
-runningTask();
-console.log('Hello World');
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+    // runningTask();
+    console.log('Hello World');
+})
+
+
+
 
